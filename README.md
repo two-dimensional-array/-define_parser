47c919c7f06a85de4f40172edcebb8082a4e8503
two-dimensionall-array <andreykolomiets630@gmail.com>
2023-08-06 13:20:11 +0300

    Define parser was updated.

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 Extract macro values (#define) from the header file and store in a readable format.
 ---
 ## Command Line Arguments
-- **-i | --input**  - Path to the header file that should be parsed.
-- **-j | --json**   - Path to JSON file, which contain all parsed macro names.
-- **-o | --output** - Path to output file.
+- **-h | --header**     - Path to the header file that should be parsed.
+- **-j | --json**       - Path to JSON file, which contain all parsed macro names.
+- **-h | --flags**      - Compilation flags.
+- **-i | --include**    - Path to include path file.
+- **-d | --define**     - Path to defines file.
+- **-s | --substitute** - Path to file, in which need substitute defines values.
 ---
 ## Startup Define Parser script
 Example of command startup [define parser][define_parser.py] script. In this repository example of command startup is located in [Makefile][startup_command] file.
 ```bash
-~/repository_directory/python define_parser.py -i header.h -j defines.json -o main.c
+~/repository_directory/python define_parser.py -h header.h -j defines.json -s main.c
 ```
 ## Example of output JSON file
 In this repository example of JSON file is [defines.json][defines.json] file.
--- define_parser.py
+++ define_parser.py
@@ -3,10 +3,12 @@ import json
 import sys
 import getopt
 from typing import IO
+import subprocess
 
 DEFINE_PATTERN = re.compile(r'^[\t ]*#[\t ]*define[\t ]+([a-zA-Z0-9_]+)(?:\s+|\Z)')
 C_COMMENT_PATTERN = re.compile(r'/\*([^*]+|\*+[^/])*(\*+/)?')
 CPP_COMMENT_PATTERN = re.compile(r'//.*')
+VAR_PATTERN =  re.compile(r'^\t* *auto +var_([a-zA-Z0-9_]+) *= *(.+);')
 
 def get_defines(fp: IO) -> dict:
     defines = {}
@@ -30,38 +32,87 @@ def sub_defines_to_string(defines: dict, string: str) -> str:
         if not value is None:
             string = re.sub(f'(?<=[^\d\w_])({macro_name})(?=[^\d\w_])', value, string)
 
-    return string;
+    return string
 
 def main(argv: list[str]):
     header_file = ""
-    defines_file = ""
-    output_file = ""
+    json_file = ""
+    include_file = ""
+    define_file = ""
+    substitute_file = ""
     defines_buffer = {}
+    cmd = "g++ -E defines.cpp -o defines.out"
 
     # Processing command line arguments.
-    opts, args = getopt.getopt(argv, "i:j:o:", ["input=", "json=", "output="])
+    opts, args = getopt.getopt(argv, "h:j:f:i:d:", ["header=", "json=", "flags=", "include=", "define=", "substitute="])
     for opt, arg in opts:
-        if opt in ("-i", "--input"):
+        if opt in ("-h", "--header"):
             header_file = arg
         elif opt in ("-j", "--json"):
-            defines_file = arg
-        elif opt in ("-o", "--output"):
-            output_file = arg
+            json_file = arg
+        elif opt in ("-f", "--flags"):
+            cmd += f" {arg}"
+        elif opt in ("-i", "--include"):
+            include_file = arg
+        elif opt in ("-d", "--define"):
+            define_file = arg
+        elif opt in ("-s", "--substitute"):
+            substitute_file = arg
 
     # Parsing the header file and extracting all macro names (#defines) into buffer.
     with open(header_file, "r") as header:
         defines_buffer = get_defines(header)
+    
+    # Generate source file
+    with open("./defines.cpp", "w") as build:
+        build.write(f"#include \"{header_file}\"\n\n")
+        build.write("int main()\n")
+        build.write("{\n")
+        for define in defines_buffer.keys():
+            if defines_buffer.get(define) != None:
+                build.write(f"\tauto var_{define} = {define};\n")
+        build.write("\treturn 0;\n")
+        build.write("}\n")
+
+    # Add include files path to compilation command
+    if include_file != "":
+        with open(include_file, "r") as include:
+            for line in include.readlines():
+                cmd += f" -I \"{line}\""
+
+    # Add defines to compilation command
+    if define_file != "":
+        with open(define_file, "r") as define:
+            for line in define.readlines():
+                cmd += f" -D {line}"
+
+    # Generate defines values by precompiled file
+    subprocess.Popen(cmd, shell=True).wait()
+
+    # Get defines values from precompiled file
+    with open("./defines.out", "r") as pre_build:
+        for line in pre_build.readlines():
+            match = VAR_PATTERN.match(line)
+            if not match is None:
+                macro_name = match.group(1)
+                value = match.group(2)
+                defines_buffer.update({macro_name : value})
+
+    # Delete temporary files
+    subprocess.Popen("rm defines.cpp defines.out", shell=True)
 
     # Generation a json with all macro names (#defines).
-    with open(defines_file, "w") as out:
-        json.dump(defines_buffer, out, indent=1)
-    
+    if json_file != "":
+        with open(json_file, "w") as out:
+            json.dump(defines_buffer, out, indent=1)
+
     # Modifying existing output source file to use all parsed macro names.
-    with open(output_file, "r+") as output:
-        output_text = output.read()
-        output.seek(0)
-        output.truncate(0)
-        output.write(sub_defines_to_string(defines_buffer, output_text))
+    if substitute_file != "":
+        with open(substitute_file, "r+") as substitute:
+            substitute_text = substitute.read()
+            substitute.seek(0)
+            substitute.truncate(0)
+            substitute.write(sub_defines_to_string(defines_buffer, substitute_text))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
